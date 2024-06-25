74394c96b1f5cc1aa5dad8a4249473d40126d6d1
Murilo Perosa <perosamurilo@gmail.com>
2024-06-25 18:20:28 -0300

    feat: loops.

---
+++ loops.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
+func main() {
+
+	// make slices
+	userNames := make([]string, 2, 4) // len, cap
+	userNames[0] = "Bernardo"
+	userNames[1] = "Juno"
+	// userNames[2] = "Simone" // error
+	userNames = append(userNames, "Murilo")
+	userNames = append(userNames, "Rafaela")
+
+	userNames = append(userNames, "Gilmar") // re-allocate cap
+
+	fmt.Println(userNames)
+
+	// make maps
+	courseRatings := make(floatMap, 3) // cap
+	courseRatings["go"] = 10
+	courseRatings["php"] = 10
+	courseRatings["js"] = 9
+
+	courseRatings["vue"] = 8 // re-allocate cap
+
+	courseRatings.output()
+
+	for index, value := range userNames {
+		fmt.Println(index, value)
+	}
+
+	for index, value := range courseRatings {
+		fmt.Println(index, value)
+	}
+}
